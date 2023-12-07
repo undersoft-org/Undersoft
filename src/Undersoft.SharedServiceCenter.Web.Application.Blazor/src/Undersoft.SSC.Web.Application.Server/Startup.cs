17d1142bf818d6b1e5c836c41968a0a1b9a8b97e
daha <dh@undersoft.pl>
2023-12-07 09:45:52 +0100

    .

---
+++ src/Undersoft.SharedServiceCenter.Web.Application.Blazor/src/Undersoft.SSC.Web.Application.Server/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Undersoft.SDK.Service.Application;
+using Undersoft.SDK.Service.Application.DataServer;
+
+namespace Undersoft.SSC.Web.Application.Server;
+
+using Infrastructure.Persistance.Stores;
+using Undersoft.SDK.Service.Data.Store;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services
+            .AddApplicationSetup()
+            .ConfigureApplication(true, AppDomain.CurrentDomain.GetAssemblies())        
+            .AddDataServer<IEventStore>(
+                DataServerTypes.All,
+                builder =>
+                    builder.AddDataServices<AppEventStore>()
+            );
+        services.AddControllersWithViews();
+        services.AddRazorPages();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseWebAssemblyDebugging();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseBlazorFrameworkFiles();
+
+        app.UseInternalProvider()
+            .UseApplicationSetup(env, true, true)
+            .UseDataMigrations()
+            .UseDataServices()
+            .MapFallbackToFile("/index.html");
+    }
+}
---
+++ src/Undersoft.SoftwareDevelopmentKit/src/Undersoft.SDK.Service/Data/Service/ActionServiceKind.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentValidation.Results;
+
+namespace Undersoft.SDK.Service.Data
+{
+    public enum ActionServiceKind
+    {
+        None,
+        SignIn,
+        SignUp,
+        SetPassword,
+        SetEmail,
+        ConfirmPassword,
+        ConfirmEmail,
+        CompleteRegistration,
+        Token
+    }
+}
