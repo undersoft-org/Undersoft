c0967679850119556de65e0ad23cfee3f37b6dd6
daha <dh@undersoft.pl>
2024-02-23 19:21:21 +0100

    .

--- src/SharedServiceCenter/src/Undersoft.SSC.Service.Application.GUI/Compound/Landing/TopMenu/Account/AccountMenu.cs
+++ src/SharedServiceCenter/src/Undersoft.SSC.Service.Application.GUI/Compound/Landing/TopMenu/Account/AccountMenu.cs
@@ -9,9 +9,15 @@ public class AccountMenu : DataObject
     public ProfileMenu Profile { get; set; } = new ProfileMenu();
 
     [VisibleRubric]
+    [DisplayRubric("Sign up")]
+    public SignUpMenu SignUp { get; set; } = new SignUpMenu();
+
+    [VisibleRubric]
+    [DisplayRubric("Sign in")]
     public SignInMenu SignIn { get; set; } = new SignInMenu();
 
     [VisibleRubric]
+    [DisplayRubric("Sign out")]
     public SignOutMenu SignOut { get; set; } = new SignOutMenu();
 }
 
